2e3c9b0425af2c4f790970bb28ba9a10490ee064
Amanda Esteves <amandaestevs@gmail.com>
2023-05-05 20:38:23 -0300

    Removed env file

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,6 @@ export default function Home() {
     <main className="min-h-screen w-full flex flex-col items-center justify-center gap-y-10 px-6 py-7 bg-main-img bg-cover">
          <Searchbar />
          <PersonalizedSearch />
-    </main>
+    </main> 
   )
 }
